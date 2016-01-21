92230a0059eb75768c607c9b70bffc5cc237c99e
Chad Cunningham <ccunningham2434@mail.kvcc.edu>
2016-01-20 19:52:44 -0500

    Employee class. Update program to use Employee.

---
+++ cis237inclass1/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass1
+{
+    class Employee
+    {
+        // >Fields.
+        private string firstName;
+        private string lastName;
+        private decimal weeklySalary;
+
+
+        // >Properties.
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public decimal WeeklySalary
+        {
+            get { return weeklySalary; }
+            set { weeklySalary = value; }
+        }
+
+
+        // >Methods.
+        public string GetFullName()
+        {
+            return this.firstName + " " + this.lastName;
+        }
+
+        public override string ToString()
+        {
+            return this.firstName + " " + this.lastName + " " + this.weeklySalary.ToString("C");
+        }
+
+        // >Constructors.
+        public Employee(string firstName, string lastName, decimal weeklySalary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.WeeklySalary = weeklySalary;
+        }
+        // >Default Constructor.
+        public Employee()
+        {
+
+        }
+
+    }
+}
--- cis237inclass1/Program.cs
+++ cis237inclass1/Program.cs
@@ -10,12 +10,62 @@ namespace cis237inclass1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Here is some text.");
-            Console.WriteLine("Here is more some text.");
+            Employee employee0 = new Employee("Chad", "Cunningham", 600.00m);
+            Employee employee1 = new Employee("Joe", "Somebody", 5.00m);
 
-            Console.WriteLine("Text for another commit.");
+            int myNumber = 5;
+
+            Console.WriteLine(myNumber);
+            ChangeAnInt(myNumber);
+            Console.WriteLine(myNumber);
+
+            Console.WriteLine(employee0.ToString());
+            ChangeAnObject(employee0);
+            Console.WriteLine(employee0.ToString());
+
+
+
+
+            //Console.WriteLine(employee0.GetFullName());
+            //Console.WriteLine(employee0.ToString());
+
+            // >Array of objects.
+            Employee[] employeeArray = new Employee[10];
+
+            // >Instanciate some employees into the array.
+            employeeArray[0] = new Employee("Darren", "Froberg", 765.00m);
+            employeeArray[1] = new Employee("Stan", "Marsh", 532.00m);
+            employeeArray[2] = new Employee("Kyle", "Broflowskie", 223.00m);
+            employeeArray[3] = new Employee("Eric", "Cartman", 142.00m);
+            employeeArray[4] = new Employee("Kenny", "Something", 2.00m);
+
+            // >Display everything in the array.
+            foreach (Employee employee in employeeArray)
+            {
+                if (employee != null)
+                {
+                    Console.WriteLine(employee.ToString());
+                }
+            }
+
         }
-        
+
+        // >Changes an integer.
+        static void ChangeAnInt(int myNumber)
+        {
+            myNumber = 456;
+        }
+        // >Changes an object. Objects are passed by reference.
+        static void ChangeAnObject(Employee employee)
+        {
+            employee.FirstName = "Thor";
+        }
+
+
+
+
+
+
 
     }
     
